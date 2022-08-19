c886f5fde6c18dcd16e707d3e87a00098fc16071
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-08-19 16:16:10 +0300

    Completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(n) {
+    if ( n<42) {
+        return (42-n);
+    } else {
+        return (n-42);
+    }
+}
+
+function distanceFromHqInFeet(x) {
+    return distanceFromHqInBlocks(x)*264;
+
+}
+
+function distanceTravelledInFeet(a,b) {
+    if (b>a) {
+        return ((b-a)*264);
+    }
+    else 
+    return ((a-b)*264);
+}
+
+function calculatesFarePrice(start,destination) {
+    let distance = distanceTravelledInFeet(start,destination);
+    if (distance<= 400) {
+        return 0;
+    }
+    if (distance>=400 && distance<=2000) {
+        return (distance-400)* 0.02;
+    }
+    else if (distance>=2000&& distance<=2500) {
+        return 25;
+    }
+    else if (distance > 2500) {
+        return "cannot travel that far";
+    }
+}
